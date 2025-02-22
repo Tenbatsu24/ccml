9a39e2e5d2c05cb8b2282d9e802814a6cd61fb3c
Tenbatsu24 <puruvaish24@outlook.com>
2025-02-22 23:29:10 +0100

    change: custom formatting to include file path and replace PROJ_ROOT in logs with "." to anonymize paths

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
@@ -6,6 +6,48 @@ from loguru import logger
 # Load environment variables from .env file if it exists
 load_dotenv()
 
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+
+
+def _format_message(record):
+    # process record to generate
+    t = record["time"]
+    level = record["level"]
+
+    file = record["file"]
+    function = record["function"]
+    line = record["line"]
+
+    message = record["message"]
+
+    # escape tags in message, tags are "<some_tag>"
+    if function.startswith("<") and function.endswith(">"):
+        function = rf"\{function}"
+
+    message.replace("<", r"\<")
+
+    # strip the path to the project root and replace it with a dot for brevity in both file and message
+    file = str(file.path).replace(str(PROJ_ROOT), "$PROJECT_ROOT")
+    message = message.replace(str(PROJ_ROOT), "$PROJECT_ROOT")
+
+    return (
+        f"<green>{t:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+        f"<level>{level: <8}</level> | "
+        f"<cyan>{file}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>\n"
+    )
+
+
+logger.remove(0)
+
+# If tqdm is installed, configure loguru with tqdm.write
+# https://github.com/Delgan/loguru/issues/135
+try:
+    from tqdm import tqdm
+
+    logger.add(lambda msg: tqdm.write(msg, end=""), format=_format_message, colorize=True)
+except ModuleNotFoundError:
+    logger.add(sys.stderr, format=_format_message, colorize=True)
+
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")

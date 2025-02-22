23aeea9d3573d8df9c741d8a21419d385649aeb2
Tenbatsu24 <puruvaish24@outlook.com>
2025-02-22 23:30:21 +0100

    change: constants initialization with defaults.

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/config.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -49,26 +52,50 @@ except ModuleNotFoundError:
     logger.add(sys.stderr, format=_format_message, colorize=True)
 
 # Paths
-PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
-DATA_DIR = PROJ_ROOT / "data"
+# Data directories
+DATA_DIR = os.getenv("DATA_DIR", PROJ_ROOT / "data")
+logger.info(f"DATA_DIR path is: {DATA_DIR}")
+
+SAMPLE_DATA_DIR = os.getenv("SAMPLE_DATA_DIR", DATA_DIR / "sample")
+logger.info(f"SAMPLE_DATA_DIR path is: {SAMPLE_DATA_DIR}")
+
+CONFIGS_DIR = os.getenv("CONFIGS_DIR", PROJ_ROOT / "configs")
+logger.info(f"CONFIGS_DIR path is: {CONFIGS_DIR}")
+
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
-MODELS_DIR = PROJ_ROOT / "models"
+# Model directories
+MODELS_DIR = os.getenv("MODELS_DIR", PROJ_ROOT / "models")
 
-REPORTS_DIR = PROJ_ROOT / "reports"
+# Report directories
+REPORTS_DIR = os.getenv("REPORTS_DIR", PROJ_ROOT / "reports")
 FIGURES_DIR = REPORTS_DIR / "figures"
 
-# If tqdm is installed, configure loguru with tqdm.write
-# https://github.com/Delgan/loguru/issues/135
-try:
-    from tqdm import tqdm
+LOGS_DIR = os.getenv("LOGS_DIR", PROJ_ROOT / "logs")
+logger.info(f"LOGS_DIR path is: {LOGS_DIR}")
 
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-except ModuleNotFoundError:
-    pass
+# ML Logging and Monitoring
+WANDB_ENTITY = os.getenv("WANDB_ENTITY", None)
+if WANDB_ENTITY is None:
+    logger.info("WANDB_ENTITY is not set. This will log to the default entity.")
+else:
+    logger.info(f"WANDB_ENTITY is: {WANDB_ENTITY}")
+
+WANDB_PROJECT = os.getenv("WANDB_PROJECT", None)
+if WANDB_PROJECT is None:
+    logger.warning(
+        f"WANDB_PROJECT is not set. Defaulting to: {Path(__file__).resolve().parents[1].name}"
+    )
+    WANDB_PROJECT = Path(__file__).resolve().parents[1].name
+else:
+    logger.info(f"WANDB_PROJECT is: {WANDB_PROJECT}")
+
+if os.getenv("WANDB_MODE") == "disabled":
+    logger.info("WANDB_MODE is disabled. Disabling WandB logging.")
+    # set WANDB_SILENT to true to disable logging
+    os.environ["WANDB_SILENT"] = "true"
+    logger.info("WandB is made silent.")
